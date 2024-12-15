05322c268415cd738b7584391bc5b4c679a9b435
fe-berrios <fe.berrios.ov@gmail.com>
2024-12-14 22:41:04 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ceremoniaXLS',
+  webDir: 'www'
+};
+
+export default config;
